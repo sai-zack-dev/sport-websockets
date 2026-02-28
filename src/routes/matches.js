1670a441817404db64cb72009e97462cbe67a027
sai-zack-dev <saizlinh@gmail.com>
2026-02-28 13:15:57 +0700

    implement the websocket server

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -63,6 +63,11 @@ matchRouter.post("/", async (req, res) => {
         status: getMatchStatus(startTime, endTime),
       })
       .returning();
+
+    if(res.app.locals.broadcastMatchCreated) {
+      res.app.locals.broadcastMatchCreated(event);
+    }
+
     res.status(201).json({ data: event });
   } catch (error) {
     res.status(500).json({
